c8a19b2ac16c11b5b736bc61cd52404496c9a094
Jody Almaida Putra <jody.almaida@gmail.com>
2018-09-08 22:39:33 +0700

    halaman depan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Fitur Teknologi Test</title>
+        <title>Manifa Laundry</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Fitur Teknologi Test
+                    <img class="logo" src="{{ asset('template/images/manifa.jpg') }}" height="70%" width="70%">
                 </div>
 
             </div>
